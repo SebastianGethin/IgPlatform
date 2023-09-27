440ba011bd8ab9ab96cc34b2aa11f23fceb34c47
Sebastian <144529350+SebastianGethin@users.noreply.github.com>
2023-09-27 11:45:15 +0100

    Update trading_ig_config.py

--- trading_ig_config.py
+++ trading_ig_config.py
@@ -1,3 +1,9 @@
+'''
+This is a template for the trading_ig_config file which holds authentication credentials. It is required for the API handler to function.
+The first step in using the files in this repository is to download this template, populate the fields with your credentials (available
+from ig.com) and save it to your project folder.
+'''
+
 class api_config(object):
     username = ''
     password = ''
